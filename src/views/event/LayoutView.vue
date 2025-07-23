65daad759bcd71bdd7b4a02aab890d72a20d4e34
Thanathorn31 <thanathorn_tee@cmu.ac.th>
2025-07-24 01:09:59 +0700

    3.3

--- src/views/event/LayoutView.vue
+++ src/views/event/LayoutView.vue
@@ -7,13 +7,13 @@ import EventService from '@/services/EventService'
 const event = ref<Event | null>(null);
 const props = defineProps({
   id: {
-    type: Number,
+    type: String,
     required: true,
   },
 })
 
 onMounted(() => {
-    //fetch event (by id) and set local event data
+   
     EventService.getEvent(parseInt(props.id))
     .then((response) => {
       event.value = response.data
@@ -29,7 +29,16 @@ onMounted(() => {
 <template>
   <div v-if="event">
     <h1>{{ event.title }}</h1>
-    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
-    <p>{{ event.description }}</p>
+
+ <nav>
+<router-link :to="{ name: 'event-detail-view' }">Details</router-link>
+      |
+      <router-link :to="{ name: 'event-register-view' }">Register</router-link>
+      |
+      <router-link :to="{ name: 'event-edit-view' }">Edit</router-link>
+    </nav>
+    <router-view :event="event"></router-view>
+
+
   </div>
 </template>
