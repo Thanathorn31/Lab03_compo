24e5040293efbbbed321935115f91369958a2e5d
Thanathorn31 <thanathorn_tee@cmu.ac.th>
2025-07-23 17:14:03 +0700

    initial commit

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/Thanathorn31/Lab02_compo',
+  baseURL: 'https://my-json-server.typicode.com/Thanathorn31/Lab03_compo',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
